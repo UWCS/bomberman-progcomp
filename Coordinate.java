883b69764aba025f809653227ec98601f783ec89
RuthRainbow <ruth.king1991@gmail.com>
2012-07-22 13:09:25 +0100

    Forgot the Coordinate class

---
+++ Coordinate.java
@@ -0,0 +1,31 @@
+public class Coordinate {
+	private int x;
+	private int y;
+	
+	public Coordinate(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	@Override
+	public String toString() {
+		return ("(" + this.x + "," + this.y + ")");
+	}
+	
+}
